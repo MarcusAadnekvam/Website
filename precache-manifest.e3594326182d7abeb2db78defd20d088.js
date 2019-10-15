76e09b1e44500d7b4d137354f00773bf3c6dad81
AadnekvamApps <maadnekvam@hotmail.com>
2019-10-15 15:25:38 +0200

    Updates

---
+++ precache-manifest.e3594326182d7abeb2db78defd20d088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85949eb143f3fe75ea76816d21fb3a2e",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "dd7e43598e59ba48eb0a",
+    "url": "/Website/static/css/main.616a20f0.chunk.css"
+  },
+  {
+    "revision": "af960e053e4a3b079743",
+    "url": "/Website/static/js/2.3b7f6f23.chunk.js"
+  },
+  {
+    "revision": "dd7e43598e59ba48eb0a",
+    "url": "/Website/static/js/main.a11bf92d.chunk.js"
+  },
+  {
+    "revision": "4cae33e8ee63d38c4f7e",
+    "url": "/Website/static/js/runtime-main.94847ad1.js"
+  },
+  {
+    "revision": "ff4fe815b69976c6be5953fe3c9c0419",
+    "url": "/Website/static/media/squadW.ff4fe815.jpg"
+  }
+]);
